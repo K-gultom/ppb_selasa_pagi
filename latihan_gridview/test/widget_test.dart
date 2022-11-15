b73c4abde3bc9f899131dbbc252994ed29d67464
K-gultom <yehezkielgg28@gmail.com>
2022-11-15 11:06:27 +0700

    pertemuan 7

--- latihan_gridview/test/widget_test.dart
+++ latihan_gridview/test/widget_test.dart
@@ -8,13 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:latihan_listview/main.dart';
+import 'package:latihan_gridview/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget( MyApp());
-    //sebelum MyApp, di line 16 tambahkan const
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
